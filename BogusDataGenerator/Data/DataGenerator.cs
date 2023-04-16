1f83f50595d899c081ad529a0d8f2370a7924140
ShellyWonder <91100506+ShellyWonder@users.noreply.github.com>
2023-04-16 09:51:13 -0500

    Add project files.

---
+++ BogusDataGenerator/Data/DataGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Bogus;
+
+namespace BogusDataGenerator.Data
+{
+    public class DataGenerator
+    {
+        Faker<AERecordModel> AERecordModelFaker;
+
+        public DataGenerator()
+        {
+            Randomizer.Seed = new Random(123);
+
+
+            AERecordModelFaker = new Faker<AERecordModel>()
+            .RuleFor(u => u.Id, f => f.Random.Int(1, 10000))
+            .RuleFor(ae => ae.AEId, f => f.Random.AlphaNumeric(6))
+            .RuleFor(ae => ae.EmployeeId, f => f.Random.Number(1000, 50000).ToString())   
+            .RuleFor(ae => ae.FirstName, f => f.Name.FirstName())
+            .RuleFor(ae => ae.LastName, f => f.Name.LastName())
+            .RuleFor(ae => ae.JobTitle, f => f.Name.JobTitle())
+            .RuleFor(ae => ae.Email, f => f.Internet.Email())
+            .RuleFor(ae => ae.Phone, f => f.Phone.PhoneNumber())
+            .RuleFor(ae => ae.HomeOfficeStreetAddress, f => f.Address.StreetAddress())
+            .RuleFor(ae => ae.City, f => f.Address.City())
+            .RuleFor(ae => ae.State, f => f.Address.StateAbbr())
+            .RuleFor(ae => ae.ZipCode, f => f.Address.ZipCode())
+            .RuleFor(ae => ae.EmploymentDate, f => f.Date.Past());
+        }
+        public AERecordModel GenerateAERecord()
+        {
+            return AERecordModelFaker.Generate();
+        }
+
+        public IEnumerable<AERecordModel> GenerateAERecords(int count)
+        {
+            return AERecordModelFaker.GenerateForever();
+        }
+    }
+
+
+}
